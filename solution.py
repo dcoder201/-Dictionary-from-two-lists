3fa9f032bf2f2eabe3f8c025ec321d7da6600aa5
D-coder <110387042+dcoder201@users.noreply.github.com>
2023-01-01 14:09:20 +0530

    Create solution.py

---
+++ solution.py
@@ -0,0 +1,26 @@
+from itertools import zip_longest
+def create_dict(keys, values):
+    d=dict()
+    res=zip_longest(keys,values)
+    for i,j in res:
+        if i!=None:
+            d[i]=j
+    return d
+
+ 
+from solution import create_dict
+import codewars_test as test
+
+@test.describe("Basic tests")
+def test_group():
+    
+    # Add your tests on specified format:
+    # i: ((inp_keys, inp_values), expected)
+    tests = {
+        0: ((['a', 'b', 'c', 'd'], [1, 2, 3]),{'a': 1, 'b': 2, 'c': 3, 'd': None}),
+        1: ((['a', 'b', 'c'], [1, 2, 3, 4]),{'a': 1, 'b': 2, 'c': 3})
+    }
+    for _, value in tests.items():
+        @test.it(f"Testing for keys = {value[0][0]}, values= {value[0][1]}, expected = {value[1]}")
+        def test_case():
+            test.assert_equals(create_dict(value[0][0], value[0][1]), value[1], f"Didnt work for  keys = {value[0][0]}, values= {value[0][1]}")
